eb72bdb60c46d3cc9b72a6ab5befff1ad5e27d39
Maciej <maciej.owczarski69@gmail.com>
2025-03-06 14:34:12 +0100

    second snapshot

--- first-commit.py
+++ first-commit.py
@@ -1,1 +1,17 @@
 import os
+import time
+import psutil
+
+cpu = psutil.cpu_percent(interval=1)
+print(f"CPU usage:{cpu}%") 
+#this variable collects data about cpu usage by calculating 
+#it over 1-second interval
+
+memory = psutil.virtual_memory()
+print(f"Memory Usage: {memory.percent}%")
+#this variable collects data about memory usage
+
+disk = psutil.disk_usage('/')
+print(f"Disk Usage: {disk.percent}%")
+#this variable collects data about disk usage 
+
