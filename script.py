d4fddbf274a1fd638f4779a8c6cc23c6af3fa228
Maciej <maciej.owczarski69@gmail.com>
2025-03-10 14:39:51 +0100

    added logs.txt file, forwarded data about system usage to logs.txt file

--- script.py
+++ script.py
@@ -2,23 +2,30 @@ import os
 import time
 import psutil
 
-tick=0 #variable holding tick number
-while (tick<=12):
-
+lg="logs.txt"
+file = open(lg,'a')
+seconds =time.time()
+local_time=time.ctime(seconds)
+file.write(f"\n Time of starting the script " + local_time + "\n")
+tick=1 #variable holding tick number
+while (tick<=5):
     cpu = psutil.cpu_percent(interval=1)
-    print(f"CPU usage:{cpu}%") 
+    file.write(f"CPU usage:{cpu}% \n") 
+    print(f"CPU usage:{cpu}%")
     #this variable collects data about cpu usage by calculating 
     #it over 1-second interval
 
     memory = psutil.virtual_memory()
     print(f"Memory Usage: {memory.percent}%")
+    file.write(f"Memory Usage: {memory.percent}% \n")
     #this variable collects data about memory usage
 
     disk = psutil.disk_usage('/')
+    file.write(f"Disk Usage: {disk.percent}% \n")
     print(f"Disk Usage: {disk.percent}%")
     #this variable collects data about disk usage
 
 
     tick = tick +1 #incrementing tick
-    print (tick)
-    time.sleep(5) #Pause for 5 second
+    time.sleep(1) #Pause for 5 second
+file.close()
