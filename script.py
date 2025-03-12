154786b3faf8417156f1a63e507fa763617aab4f
Maciej <maciej.owczarski69@gmail.com>
2025-03-12 15:27:19 +0100

    Sixth snapshot

--- script.py
+++ script.py
@@ -10,8 +10,7 @@ file.write(f"\n Time of starting the script " + local_time + "\n")
 tick=1 #variable holding tick number
 data=[] #initialazing empty list
 
-
-while (tick<=10): # loop by default will gather data for 10 seconds
+while (tick<=10): # loop by default will gather data for 5 ticks, tick=15seconds
     cpu = psutil.cpu_percent(interval=1)
     file.write(f"CPU usage:{cpu}% \n") 
     #this variable collects data about cpu usage by calculating 
@@ -24,10 +23,14 @@ while (tick<=10): # loop by default will gather data for 10 seconds
     disk = psutil.disk_usage('/')
     file.write(f"Disk Usage: {disk.percent}% \n")
     #this variable collects data about disk usage
-    data.append([cpu, memory.percent, disk.percent]) #appends all data to list
+    Timestamp=pd.Timestamp.now().strftime('%H:%M:%S')
+    data.append([Timestamp,cpu, memory.percent, disk.percent]) #appends all data to list
 
     tick = tick +1 #incrementing tick
-    time.sleep(1) #Pause for 5 second
-df1 = pd.DataFrame(data, columns=['Cpu Usage (%)','Memory Usage (%)','Disk usage (%)']) # this variable holds system monitor data
+    time.sleep(1) #Pause
+
+df1 = pd.DataFrame(data,columns=['Timestamp','Cpu Usage (%)','Memory Usage (%)','Disk usage (%)'])
+df1.set_index('Timestamp', inplace=True)
+# this variable holds system monitor data
 print(df1) #printing table in the terminal
 file.close()
