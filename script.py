ee1785b05a2bdb3b6b999df2a5cc06f7a9d64169
Maciej <maciej.owczarski69@gmail.com>
2025-03-17 13:41:23 +0100

    eight snapshot

--- script.py
+++ script.py
@@ -10,7 +10,7 @@ file.write(f"\n Time of starting the script " + local_time + "\n")
 tick=1 #variable holding tick number
 data=[] #initialazing empty list
 
-while (tick<=10): # loop by default will gather data for 5 ticks, tick=15seconds
+while (tick<=60): # loop by default will gather data for 5 ticks, tick=15seconds
     cpu = psutil.cpu_percent(interval=1)#this variable collects data about cpu usage by calculating it over 1-second interval
     file.write(f"CPU usage:{cpu}% \n") 
     
@@ -28,7 +28,7 @@ while (tick<=10): # loop by default will gather data for 5 ticks, tick=15seconds
     data.append([Timestamp,cpu, memory.percent, disk.percent,memory_MB,disk_MB]) #appends all data to list
 
     tick = tick +1 #incrementing tick
-    time.sleep(1) #Pause
+    time.sleep(30) #Pause
 
 df1 = pd.DataFrame(data,columns=['Timestamp','Cpu Usage (%)','Mem Usage (%)','Disk usage (%)','Mem Usage (MB)','Disk Usage (MB)'])
 # this variable holds system monitor data
