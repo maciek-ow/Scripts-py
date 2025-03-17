dc2445e60983d3fb4bd22a9fc6aefe2b8d167e7f
Maciej <maciej.owczarski69@gmail.com>
2025-03-17 13:51:56 +0100

    nine snapshot

--- script.py
+++ script.py
@@ -15,13 +15,13 @@ while (tick<=60): # loop by default will gather data for 5 ticks, tick=15seconds
     file.write(f"CPU usage:{cpu}% \n") 
     
     memory = psutil.virtual_memory()#this variable collects data about memory usage
-    file.write(f"Memory Usage: {memory.percent}% \n")
+    file.write(f"Memory Usage: {memory.percent}% \n") #this variable holds memory usage in Bytes and its converteted to MegaBytes afterwads
     memory_MB = psutil.virtual_memory().used / (1024 ** 2)
     memory_MB = round(memory_MB)
 
     disk = psutil.disk_usage('/')#this variable collects data about disk usage
     file.write(f"Disk Usage: {disk.percent}% \n")
-    disk_MB = psutil.disk_usage('/').used / (1024 ** 2)
+    disk_MB = psutil.disk_usage('/').used / (1024 ** 2) #this variable holds disk usage in Bytes and its converteted to MegaBytes afterwads
     disk_MB = round(disk_MB,2)
 
     Timestamp=pd.Timestamp.now().strftime('%H:%M:%S')
